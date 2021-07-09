17e73b575b76c071356a025f21b61bc3971f9f28
Leon <leonwheeler08@gmail.com>
2021-07-09 15:48:29 +0100

    Added SASS, changed wave animation, changes colours

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import Header from "./components/Header";
 
 function App() {
@@ -6,6 +6,11 @@ function App() {
     <div className="App">
       <div>
         <Header />
+      </div>
+      <div>
+        <div className="projects d-flex justify-content-center">
+          <h1 className="mt-5">projects</h1>
+        </div>
       </div>
     </div>
   );
