e77ef8cbc07bf062576190c05eb979cf624edad0
Leon <leonwheeler08@gmail.com>
2021-07-09 10:25:47 +0100

    Changed positioning, added title

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,13 @@
+import Navbar from "../Navbar";
 const Header = () => {
   return (
-    <header>
-      <div className="ocean d-flex align-items-center">
-        <div>
-          <h1>leon wheeler</h1>
-        </div>
-        <div class="wave"></div>
+    <header className="ocean">
+      <Navbar />
+      <div className="personalDetails">
+        <h1>leon wheeler</h1>
+        <h2>full-stack web developer</h2>
       </div>
+      <div class="wave"></div>
     </header>
   );
 };
