eb286a012b12231cf7ebb23c31ef0818caae497b
Leon <leonwheeler08@gmail.com>
2021-07-11 18:54:29 +0100

    Projects are now stored in an array and will dynamically render.

---
+++ src/assets/projectData.js
@@ -0,0 +1,13 @@
+const projects = [
+  {
+    title: "The Tech Blog",
+    description:
+      "The Tech Blog is a full-stack MVC application that allows users to create an account, login and logout, create, view, update, and delete posts, and add comments to posts.",
+    tech: "Tech Used: Node.js, Express, jQuery, MySQL, Handlebars, bCrypt, Bootstrap",
+    image: "../assets/images/homepagetech.png",
+    githubLink: "https://github.com/Leon3005/tech-blog",
+    deployedLink: "https://mighty-wildwood-56891.herokuapp.com/",
+  },
+];
+
+export default projects;
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,14 +1,30 @@
 import homepagetech from "../../assets/images/homepagetech.png";
 import Project from "./project";
+import projects from "../../assets/projectData";
 
 const Projects = () => {
+  const renderProjects = () => {
+    return projects.map((project) => {
+      return (
+        <Project
+          title={project.title}
+          description={project.description}
+          tech={project.tech}
+          image={project.image}
+          githubLink={project.githubLink}
+          deployedLink={project.deployedLink}
+        />
+      );
+    });
+  };
+
   return (
     <div>
       <h1 className="text-center" id="projectHeading">
         projects
       </h1>
       <div className="projects d-flex justify-content-evenly">
-        <Project />
+        {renderProjects()}
       </div>
     </div>
   );
